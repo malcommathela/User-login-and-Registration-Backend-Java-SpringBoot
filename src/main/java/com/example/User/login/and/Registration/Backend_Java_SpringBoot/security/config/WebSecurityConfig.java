6203081c98c1fd9143e117691ba66cc09ebce718
malcom_mathela <malcom_mathela.com>
2025-11-22 22:12:54 +0530

    SpringSecurity Patch

--- src/main/java/com/example/User/login/and/Registration/Backend_Java_SpringBoot/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/User/login/and/Registration/Backend_Java_SpringBoot/security/config/WebSecurityConfig.java
@@ -1,23 +1,59 @@
 package com.example.User.login.and.Registration.Backend_Java_SpringBoot.security.config;
 
+import com.example.User.login.and.Registration.Backend_Java_SpringBoot.appuser.App.AppUserService;
+
 import lombok.AllArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfiguration {
+
+    private final AppUserService appUserService;
+
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable) // disabling csrf using lambda syntax
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // Or your desired rules
-                );
+                        .requestMatchers("/api/v*/registration/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());  // Default form login configuration
         return http.build();
     }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(appUserService);
+        provider.setPasswordEncoder(bCryptPasswordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
